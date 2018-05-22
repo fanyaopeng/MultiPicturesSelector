ad689feda8ebc6cbe41e21fcae6db21108f07d4a
fyp <19921124>
2018-05-22 21:25:55 +0800

    11

--- app/src/main/java/com/fan/multipicturesselector/MainActivity.java
+++ app/src/main/java/com/fan/multipicturesselector/MainActivity.java
@@ -1,7 +1,16 @@
 package com.fan.multipicturesselector;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.fan.library.MultiPicturesSelectorActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void start(View view) {
+        String permission = Manifest.permission.READ_EXTERNAL_STORAGE;
+        if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{permission}, 0);
+            }
+        } else {
+            startActivity(new Intent(this, MultiPicturesSelectorActivity.class));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startActivity(new Intent(this, MultiPicturesSelectorActivity.class));
+            }
+        }
     }
 }
--- library/src/main/java/com/fan/library/MultiPicturesSelectorActivity.java
+++ library/src/main/java/com/fan/library/MultiPicturesSelectorActivity.java
@@ -9,14 +9,21 @@ import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Created by huisoucw on 2018/5/22.
@@ -29,6 +36,7 @@ public class MultiPicturesSelectorActivity extends Activity {
     private PicturesAdapter mAdapter;
     private ExecutorService mService;
     private int mItemSize;
+    private int mItemMargin;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,6 +49,7 @@ public class MultiPicturesSelectorActivity extends Activity {
         mList.setAdapter(mAdapter);
         mService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         mItemSize = getResources().getDisplayMetrics().widthPixels / 3;
+        mItemMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
     }
 
     private void init() {
@@ -55,12 +64,32 @@ public class MultiPicturesSelectorActivity extends Activity {
     private class PicturesAdapter extends RecyclerView.Adapter<PicturesAdapter.VH> {
         @Override
         public VH onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new VH(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, null));
+            return new VH(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false));
         }
 
         @Override
         public void onBindViewHolder(VH holder, int position) {
-
+            ImageView img = holder.img;
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) holder.root.getLayoutParams();
+            params.width = mItemSize;
+            params.height = mItemSize;
+            params.topMargin = mItemMargin;
+            if ((position + 1) % 3 == 1) {
+                params.leftMargin = mItemMargin;
+            } else if ((position + 1) % 3 == 2) {
+                params.leftMargin = mItemMargin;
+                params.rightMargin = mItemMargin;
+            } else {
+                params.rightMargin = mItemMargin;
+            }
+            Future<Bitmap> future = mService.submit(new CompressTask(mAllPictures.get(position)));
+            try {
+                img.setImageBitmap(future.get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
@@ -69,25 +98,39 @@ public class MultiPicturesSelectorActivity extends Activity {
         }
 
         public class VH extends RecyclerView.ViewHolder {
+            ImageView img;
+            CheckBox ck;
+            FrameLayout root;
 
             public VH(View itemView) {
                 super(itemView);
+                img = itemView.findViewById(R.id.image);
+                ck = itemView.findViewById(R.id.ck);
+                root = itemView.findViewById(R.id.root);
             }
         }
     }
 
-    private class CompressTask implements Runnable {
+    private class CompressTask implements Callable<Bitmap> {
+        private String path;
+
+        public CompressTask(String path) {
+            this.path = path;
+        }
 
         @Override
-        public void run() {
-
+        public Bitmap call() throws Exception {
+            return compress(path);
         }
     }
 
     private Bitmap compress(String path) {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
-        Bitmap src = BitmapFactory.decodeFile(path);
-        return null;
+        BitmapFactory.decodeFile(path, options);
+        int sample = Math.max(options.outWidth / mItemSize, options.outHeight / mItemSize);
+        options.inSampleSize = sample;
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(path, options);
     }
 }
